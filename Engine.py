bb5dfcefcdce79b5d8e5a67b146c58953de6eac0
Michael-Alio <michael.alio.cpp@gmail.com>
2017-02-18 12:47:14 -0800

    Updated Engine.py

--- Engine.py
+++ Engine.py
@@ -1,7 +1,6 @@
 import MoodFinder
 from spotifyArtist import spotifyEngine as se
 def get_valence():
-    print("Finding your current mood, please look at the camera for a few seconds...")
     mf = MoodFinder.MoodFinder()
     mf.setResolution(640,480);
     avg = float(mf.calculateAverageValence())
@@ -9,17 +8,18 @@ def get_valence():
     return avg
 
 if __name__=='__main__':
-    valence = 0.33#get_valence()
+    print('Welcome to mood playlists!')
+    username = input("Please enter your name: ")
+    print("Thank you,",str(username).capitalize())
+    print("Let's see how you're feeling!")
+    valence = get_valence()
     print(valence)
     spotify = se()
     name = spotify.get_user_top_artists()
-    print("Your search will be based on the artist:" ,name)
+    print("Your search will be based on the artist:" ,name,"\n")
     artist = spotify.get_artist(name)
     tracks = spotify.show_recommendations_by_valence(artist, valence)
     trackID = []
-    for track in tracks:
-        #print(track)
-        trackID.append(track['id'])
-    print(trackID)
-    spotify.add_to_playlist(trackID)
 
+    for track in tracks:
+        print(track['name'],", by ", track['artist'])
